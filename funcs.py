52cd8f1e8b3d3c93ba343070e9eaea70316e35d8
Senz1019 <Kaius10@yandex.ru>
2023-07-02 19:25:26 +0300

    Добавлено действие вычитания

---
+++ funcs.py
@@ -0,0 +1,8 @@
+def add_numbers(x, y):
+    return x + y
+    print(f'Сумма числе {x} и {y} равна {result}')
+    return result
+
+def substract_numbers(x, y):
+    return x - y
+    print(f'Разница между {x} и {y} ')
